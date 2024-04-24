71b32c93eb48c13f275eb3593353f42c0684a74d
Asiya Khassenova <bb84960@gmail.com>
2024-04-24 18:39:30 +0500

    comm

---
+++ src/main/java/com/app/TheMovie/Services/GetAPIKey.java
@@ -0,0 +1,8 @@
+package com.app.TheMovie.Services;
+
+public class GetAPIKey {
+    private final String API_Access_Key = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjRkNjcwNmZjNTkxNWRmNWE3MDk5NDQ2NjQ4MTNiZSIsInN1YiI6IjY2MjkwOGMyYjlhMGJkMDE3YWQ5ZjA4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wDj65QcgGuGXP48BCfFvqDzDtcKDw-aupgVkfSDRqp0";
+    public String getAPI_Access_Key(){
+        return API_Access_Key;
+    }
+}
